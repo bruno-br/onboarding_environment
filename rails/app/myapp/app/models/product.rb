f11980a9496ed7d5dc1bd55805d07b645689f293
Bruno Brasil <bruno.brasil@b2wdigital.com>
2021-12-03 11:47:51 -0300

    style product model

--- rails/app/myapp/app/models/product.rb
+++ rails/app/myapp/app/models/product.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
+# Product Model
 class Product
   include Mongoid::Document
+
   field :sku, type: String
   field :name, type: String
   field :description, type: String
   field :amount, type: Integer
   field :price, type: Float
+
   validates :sku, presence: true
   validates :name, presence: true
   validates :description, presence: true
@@ -15,8 +18,7 @@ class Product
 
   def as_json(*_args)
     h = super()
-    id = h.delete '_id' if h.key?('_id')
-    h['id'] = id.to_s
+    h['id'] = h.delete('_id').to_s
     h.as_json(only: %w[id sku name description amount price])
   end
 end
