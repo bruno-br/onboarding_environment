1928eec6462c30588d94c65254d56cbfeff6dddb
Bruno Brasil <bruno.brasil@b2wdigital.com>
2021-12-07 18:12:18 -0300

    add autogenerate primary key for product

--- phoenix/app/myapp/lib/myapp/management/product.ex
+++ phoenix/app/myapp/lib/myapp/management/product.ex
@@ -2,6 +2,8 @@ defmodule Myapp.Management.Product do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:id, :binary_id, autogenerate: true}
+
   schema "products" do
     field :amount, :integer
     field :description, :string
