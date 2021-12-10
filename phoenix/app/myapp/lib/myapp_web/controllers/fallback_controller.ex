e97111003c495d88a8ea11975e16f669e847598d
Bruno Brasil <bruno.brasil@b2wdigital.com>
2021-12-10 13:57:08 -0300

    dont return response body on 404

--- phoenix/app/myapp/lib/myapp_web/controllers/fallback_controller.ex
+++ phoenix/app/myapp/lib/myapp_web/controllers/fallback_controller.ex
@@ -16,10 +16,7 @@ defmodule MyappWeb.FallbackController do
 
   # This clause is an example of how to handle resources that cannot be found.
   def call(conn, {:error, :not_found}) do
-    conn
-    |> put_status(:not_found)
-    |> put_view(MyappWeb.ErrorView)
-    |> render(:"404")
+    send_resp(conn, :not_found, "")
   end
 
   # Matches other errors
