d788eb5548762119575757ab8b98ea79ce9c90e1
Bruno Brasil <bruno.brasil@b2wdigital.com>
2021-12-14 14:43:25 -0300

    add redis

--- phoenix/app/myapp/mix.exs
+++ phoenix/app/myapp/mix.exs
@@ -41,7 +41,9 @@ defmodule Myapp.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:redis, "~> 0.1"},
+      {:exredis, ">= 0.2.4"}
     ]
   end
 
