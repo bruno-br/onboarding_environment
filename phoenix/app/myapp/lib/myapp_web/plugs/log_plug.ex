68ebd7b2b8765ea0ffc4a961b06d2383ce1a1201
Bruno Brasil <bruno.brasil@b2wdigital.com>
2021-12-16 17:07:35 -0300

    create plug to save logs to elasticsearch

---
+++ phoenix/app/myapp/lib/myapp_web/plugs/log_plug.ex
@@ -0,0 +1,31 @@
+defmodule MyappWeb.Plugs.LogPlug do
+  import Plug.Conn
+
+  import Tirexs.HTTP
+
+  def init(props) do
+    props
+  end
+
+  def call(conn, _opts) do
+    post_log(conn)
+    conn
+  end
+
+  defp post_log(conn), do: post("/my_index/logs", generate_log(conn))
+
+  defp generate_log(conn) do
+    date =
+      DateTime.utc_now()
+      |> DateTime.to_iso8601()
+
+    log = [
+      date: date,
+      method: conn.method,
+      req_headers: conn.req_headers,
+      body_params: conn.body_params
+    ]
+
+    log
+  end
+end
