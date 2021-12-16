799f75fc6d432195cd180ff92b0a1ad8afa532b5
Bruno Brasil <bruno.brasil@b2wdigital.com>
2021-12-16 13:49:41 -0300

    add tirexs

--- phoenix/app/myapp/mix.exs
+++ phoenix/app/myapp/mix.exs
@@ -20,7 +20,7 @@ defmodule Myapp.MixProject do
   def application do
     [
       mod: {Myapp.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :tirexs]
     ]
   end
 
@@ -43,7 +43,8 @@ defmodule Myapp.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:redis, "~> 0.1"},
-      {:exredis, ">= 0.2.4"}
+      {:exredis, ">= 0.2.4"},
+      {:tirexs, "~> 0.8"}
     ]
   end
 
