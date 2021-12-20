4e5d9aaa8a367b8af14700691ca8b9dfd80696de
Bruno Brasil <bruno.brasil@b2wdigital.com>
2021-12-20 18:41:34 -0300

    setup tests

--- phoenix/app/myapp/config/test.exs
+++ phoenix/app/myapp/config/test.exs
@@ -6,10 +6,10 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :myapp, Myapp.Repo,
-  database: "myapp_development",
+  database: "myapp_development_test",
   # database: "myapp_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+  hostname: "localhost"
+  # pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
---
+++ phoenix/app/myapp/test/myapp_web/controllers/product_controller_test.exs
@@ -0,0 +1,100 @@
+defmodule MyappWeb.ProductControllerTest do
+  use MyappWeb.ConnCase
+
+  alias Myapp.Management
+  alias Myapp.Management.Product
+
+  @create_attrs %{
+    amount: 42,
+    description: "some description",
+    name: "some name",
+    price: 120.5,
+    sku: "some sku"
+  }
+  @update_attrs %{
+    amount: 43,
+    description: "some updated description",
+    name: "some updated name",
+    price: 456.7,
+    sku: "some updated sku"
+  }
+  @invalid_attrs %{amount: nil, description: nil, name: nil, price: nil, sku: nil}
+
+  def fixture(:product) do
+    {:ok, product} = Management.create_product(@create_attrs)
+    product
+  end
+
+  setup %{conn: conn} do
+    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+  end
+
+  describe "index" do
+    test "lists all products", %{conn: conn} do
+      conn = get(conn, Routes.product_path(conn, :index))
+      assert json_response(conn, 200)["products"] == []
+    end
+  end
+
+  describe "create product" do
+    test "renders product when data is valid", %{conn: conn} do
+      conn = post(conn, Routes.product_path(conn, :create), product: @create_attrs)
+      assert %{"id" => id} = json_response(conn, 201)["product"]
+
+      conn = get(conn, Routes.product_path(conn, :show, id))
+
+      assert %{
+               "id" => id,
+               "amount" => 42,
+               "description" => "some description",
+               "name" => "some name",
+               "price" => 120.5,
+               "sku" => "some sku"
+             } = json_response(conn, 200)["product"]
+    end
+
+    test "renders errors when data is invalid", %{conn: conn} do
+      conn = post(conn, Routes.product_path(conn, :create), product: @invalid_attrs)
+      assert json_response(conn, 422)["errors"] != %{}
+    end
+  end
+
+  describe "update product" do
+    setup [:create_product]
+
+    test "renders product when data is valid", %{conn: conn, product: %Product{id: id} = product} do
+      conn = put(conn, Routes.product_path(conn, :update, product), product: @update_attrs)
+      assert %{"id" => ^id} = json_response(conn, 200)["product"]
+
+      conn = get(conn, Routes.product_path(conn, :show, id))
+
+      assert %{
+               "id" => id,
+               "amount" => 43,
+               "description" => "some updated description",
+               "name" => "some updated name",
+               "price" => 456.7,
+               "sku" => "some updated sku"
+             } = json_response(conn, 200)["product"]
+    end
+
+    test "renders errors when data is invalid", %{conn: conn, product: product} do
+      conn = put(conn, Routes.product_path(conn, :update, product), product: @invalid_attrs)
+      assert json_response(conn, 422)["errors"] != %{}
+    end
+  end
+
+  describe "delete product" do
+    setup [:create_product]
+
+    test "deletes chosen product", %{conn: conn, product: product} do
+      conn = delete(conn, Routes.product_path(conn, :delete, product))
+      assert response(conn, 204)
+    end
+  end
+
+  defp create_product(_) do
+    product = fixture(:product)
+    %{product: product}
+  end
+end
