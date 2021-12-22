ab5f271104ad267dffde45ca1eeb5eb2e3f4434f
Bruno Brasil <bruno.brasil@b2wdigital.com>
2021-12-22 12:09:37 -0300

    add describe blocks for each function

--- phoenix/app/myapp/test/myapp/management_test.exs
+++ phoenix/app/myapp/test/myapp/management_test.exs
@@ -3,47 +3,51 @@ defmodule Myapp.ManagementTest do
 
   alias Myapp.Management
 
-  describe "products" do
-    alias Myapp.Management.Product
+  alias Myapp.Management.Product
 
-    @valid_attrs %{
-      amount: 42,
-      description: "some description",
-      name: "some name",
-      price: 120.5,
-      sku: "some-sku",
-      barcode: "123456789"
-    }
-    @update_attrs %{
-      amount: 43,
-      description: "some updated description",
-      name: "some updated name",
-      price: 456.7,
-      sku: "some-updated-sku",
-      barcode: "123456789"
-    }
-    @invalid_attrs %{amount: nil, description: nil, name: nil, price: nil, sku: nil, barcode: nil}
+  @valid_attrs %{
+    amount: 42,
+    description: "some description",
+    name: "some name",
+    price: 120.5,
+    sku: "some-sku",
+    barcode: "123456789"
+  }
+  @update_attrs %{
+    amount: 43,
+    description: "some updated description",
+    name: "some updated name",
+    price: 456.7,
+    sku: "some-updated-sku",
+    barcode: "123456789"
+  }
+  @invalid_attrs %{amount: nil, description: nil, name: nil, price: nil, sku: nil, barcode: nil}
 
-    def product_fixture(attrs \\ %{}) do
-      {:ok, product} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Management.create_product()
+  def product_fixture(attrs \\ %{}) do
+    {:ok, product} =
+      attrs
+      |> Enum.into(@valid_attrs)
+      |> Management.create_product()
 
-      product
-    end
+    product
+  end
 
-    test "list_products/0 returns all products" do
+  describe "list_products/0" do
+    test "returns all products" do
       product = product_fixture()
       assert Management.list_products() == [product]
     end
+  end
 
-    test "get_product!/1 returns the product with given id" do
+  describe "get_product!/1" do
+    test "returns the product with given id" do
       product = product_fixture()
       assert Management.get_product!(product.id) == product
     end
+  end
 
-    test "create_product/1 with valid data creates a product" do
+  describe "create_product/1" do
+    test "creates a product when data is valid" do
       assert {:ok, %Product{} = product} = Management.create_product(@valid_attrs)
       assert product.amount == 42
       assert product.description == "some description"
@@ -53,36 +57,38 @@ defmodule Myapp.ManagementTest do
       assert product.barcode == "123456789"
     end
 
-    test "create_product/1 with invalid data returns error changeset" do
+    test "returns error when data is invalid" do
       assert {:error, %Ecto.Changeset{}} = Management.create_product(@invalid_attrs)
     end
 
-    test "create_product/1 returns error when sku is invalid" do
+    test "returns error when sku is invalid" do
       product = %{@valid_attrs | sku: "invalid sku !!"}
       assert {:error, %Ecto.Changeset{}} = Management.create_product(product)
     end
 
-    test "create_product/1 returns error when name is missing" do
+    test "returns error when name is missing" do
       product = Map.delete(@valid_attrs, :name)
       assert {:error, %Ecto.Changeset{}} = Management.create_product(product)
     end
 
-    test "create_product/1 returns error when price is not greater than zero" do
+    test "returns error when price is not greater than zero" do
       product = %{@valid_attrs | price: 0}
       assert {:error, %Ecto.Changeset{}} = Management.create_product(product)
     end
 
-    test "create_product/1 returns error when barcode has less than 8 digits" do
+    test "returns error when barcode has less than 8 digits" do
       product = %{@valid_attrs | barcode: "1234567"}
       assert {:error, %Ecto.Changeset{}} = Management.create_product(product)
     end
 
-    test "create_product/1 returns error when barcode has more than 13 digits" do
+    test "returns error when barcode has more than 13 digits" do
       product = %{@valid_attrs | barcode: "12345678901234"}
       assert {:error, %Ecto.Changeset{}} = Management.create_product(product)
     end
+  end
 
-    test "update_product/2 with valid data updates the product" do
+  describe "update_product/2" do
+    test "updates the product when data is valid" do
       product = product_fixture()
       assert {:ok, %Product{} = product} = Management.update_product(product, @update_attrs)
       assert product.amount == 43
@@ -93,18 +99,22 @@ defmodule Myapp.ManagementTest do
       assert product.barcode == "123456789"
     end
 
-    test "update_product/2 with invalid data returns error changeset" do
+    test "returns error changeset when data is invalid" do
       product = product_fixture()
       assert {:error, %Ecto.Changeset{}} = Management.update_product(product, @invalid_attrs)
       assert product == Management.get_product!(product.id)
     end
+  end
 
-    test "delete_product/1 deletes the product" do
+  describe "delete_product/1" do
+    test "deletes the product" do
       product = product_fixture()
       assert {:ok, %Product{}} = Management.delete_product(product)
     end
+  end
 
-    test "change_product/1 returns a product changeset" do
+  describe "change_product/1" do
+    test "returns a product changeset" do
       product = product_fixture()
       assert %Ecto.Changeset{} = Management.change_product(product)
     end
