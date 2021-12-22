c019188305ee730de9d97c65b31dba3d46b928b1
Bruno Brasil <bruno.brasil@b2wdigital.com>
2021-12-22 17:46:51 -0300

    test: redis_api set/1 saves on cache if params are valid

---
+++ phoenix/app/myapp/test/myapp/cache/redis_api_test.exs
@@ -0,0 +1,16 @@
+defmodule Myapp.RedisApiTest do
+  use Myapp.DataCase
+
+  alias Myapp.RedisApi
+
+  @valid_key "key"
+  @valid_value "value"
+  @client RedisApi.start()
+
+  describe "set/1" do
+    test "saves on cache if params are valid" do
+      client = RedisApi.start()
+      assert RedisApi.set(client, @valid_key, @valid_value) == :ok
+    end
+  end
+end
