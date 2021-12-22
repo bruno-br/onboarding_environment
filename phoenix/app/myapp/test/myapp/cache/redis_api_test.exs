cf170abf6497845d9045c567038ca2723b554e98
Bruno Brasil <bruno.brasil@b2wdigital.com>
2021-12-22 18:08:33 -0300

    refactor redis_api tests

--- phoenix/app/myapp/test/myapp/cache/redis_api_test.exs
+++ phoenix/app/myapp/test/myapp/cache/redis_api_test.exs
@@ -3,21 +3,27 @@ defmodule Myapp.RedisApiTest do
 
   alias Myapp.RedisApi
 
-  @valid_key "key"
-  @valid_value "value"
-  @client RedisApi.start()
+  @set_params %{
+    key: "key1",
+    value: "value1"
+  }
+  @get_params %{
+    key: "key2",
+    value: "value2"
+  }
 
   describe "set/1" do
     test "saves on cache if params are valid" do
       client = RedisApi.start()
-      assert RedisApi.set(client, @valid_key, @valid_value) == :ok
+      assert RedisApi.set(client, @set_params.key, @set_params.value, 1) == :ok
     end
   end
 
   describe "get/1" do
     test "gets correct value when key is valid" do
       client = RedisApi.start()
-      assert RedisApi.get(client, @valid_key) == {:ok, @valid_value}
+      assert RedisApi.set(client, @get_params.key, @get_params.value, 1) == :ok
+      assert RedisApi.get(client, @get_params.key) == {:ok, @get_params.value}
     end
   end
 end
