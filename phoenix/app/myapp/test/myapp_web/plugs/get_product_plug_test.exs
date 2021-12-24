e046b4fb5b02e5e2e1243e48b817e5c3c55529ee
Bruno Brasil <bruno.brasil@b2wdigital.com>
2021-12-24 13:25:04 -0300

    use mock instead of creating new product

--- phoenix/app/myapp/test/myapp_web/plugs/get_product_plug_test.exs
+++ phoenix/app/myapp/test/myapp_web/plugs/get_product_plug_test.exs
@@ -1,15 +1,24 @@
 defmodule MyappWeb.Plugs.GetProductPlugTest do
   use MyappWeb.ConnCase
 
-  alias Myapp.ManagementTest
+  alias Myapp.Management
   alias MyappWeb.Plugs.GetProductPlug
+
+  import Mock
 
   describe "get product" do
     test "assigns product to conn when id is valid", %{conn: conn} do
-      product = create_product()
-      conn = get(conn, Routes.product_path(conn, :show, product.id))
+      product = "product"
 
-      assert conn.assigns[:get_product] == {:ok, product}
+      with_mock(Management,
+        get_product: fn id -> product end
+      ) do
+        conn =
+          %{conn | params: %{"id" => "existing_id"}}
+          |> GetProductPlug.call("opts")
+
+        assert conn.assigns[:get_product] == {:ok, product}
+      end
     end
 
     test "assigns not_found error when id is not found", %{conn: conn} do
@@ -19,9 +28,5 @@ defmodule MyappWeb.Plugs.GetProductPlugTest do
 
       assert conn.assigns[:get_product] == {:error, :not_found}
     end
-  end
-
-  defp create_product() do
-    ManagementTest.product_fixture()
   end
 end
