c6f4dfee10aef0920fe8f1428437e85d62d17696
Bruno Brasil <bruno.brasil@b2wdigital.com>
2021-12-27 10:18:59 -0300

    define attrs on setup_all

--- phoenix/app/myapp/test/myapp_web/plugs/get_product_plug_test.exs
+++ phoenix/app/myapp/test/myapp_web/plugs/get_product_plug_test.exs
@@ -6,25 +6,34 @@ defmodule MyappWeb.Plugs.GetProductPlugTest do
 
   import Mock
 
-  describe "get product" do
-    test "assigns product to conn when id is valid", %{conn: conn} do
-      product = "product"
+  setup_all do
+    %{
+      attrs: %{
+        product: "valid_product",
+        opts: "any_opts",
+        id: "valid_id",
+        invalid_id: "different id"
+      }
+    }
+  end
 
+  describe "get product" do
+    test "assigns product to conn when id is valid", %{conn: conn, attrs: attrs} do
       with_mock(Management,
-        get_product: fn id -> product end
+        get_product: fn id -> attrs.product end
       ) do
         conn =
-          %{conn | params: %{"id" => "existing_id"}}
-          |> GetProductPlug.call("opts")
+          %{conn | params: %{"id" => attrs.id}}
+          |> GetProductPlug.call(attrs.opts)
 
-        assert conn.assigns[:get_product] == {:ok, product}
+        assert conn.assigns[:get_product] == {:ok, attrs.product}
       end
     end
 
-    test "assigns not_found error when id is not found", %{conn: conn} do
+    test "assigns not_found error when id is not found", %{conn: conn, attrs: attrs} do
       conn =
-        %{conn | params: %{"id" => "non_existing_id"}}
-        |> GetProductPlug.call("opts")
+        %{conn | params: %{"id" => attrs.invalid_id}}
+        |> GetProductPlug.call(attrs.opts)
 
       assert conn.assigns[:get_product] == {:error, :not_found}
     end
