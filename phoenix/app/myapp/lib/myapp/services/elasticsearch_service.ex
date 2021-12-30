438d06ee670b98b6d4857ab136d7f63c581a1a29
Bruno Brasil <bruno.brasil@b2wdigital.com>
2021-12-30 16:41:03 -0300

    create function to search on elasticsearch

--- phoenix/app/myapp/lib/myapp/services/elasticsearch_service.ex
+++ phoenix/app/myapp/lib/myapp/services/elasticsearch_service.ex
@@ -6,4 +6,11 @@ defmodule Myapp.Services.ElasticsearchService do
   import Tirexs.HTTP
 
   def post(path, data), do: Tirexs.HTTP.post(path, data)
+
+  def search(path, key, value) do
+    case Tirexs.HTTP.get("#{path}/_search?q=#{key}:#{value}") do
+      {:ok, 200, search_result} -> {:ok, search_result.hits.hits}
+      _ -> {:error, :not_found}
+    end
+  end
 end
