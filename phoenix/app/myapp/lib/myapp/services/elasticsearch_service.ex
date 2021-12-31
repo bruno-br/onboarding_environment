1444cfcf44f6bd92f3bd41f2b66b6e4c2110c4fd
Bruno Brasil <bruno.brasil@b2wdigital.com>
2021-12-31 12:28:51 -0300

    create function to list all items from elasticsearch

--- phoenix/app/myapp/lib/myapp/services/elasticsearch_service.ex
+++ phoenix/app/myapp/lib/myapp/services/elasticsearch_service.ex
@@ -3,6 +3,8 @@ defmodule Myapp.Services.ElasticsearchService do
   API used to save and load data on cache using Redis
   """
 
+  @max_limit 10_000
+
   import Tirexs.HTTP
 
   def post(path, data), do: format_response(Tirexs.HTTP.post(path, data))
@@ -10,8 +12,14 @@ defmodule Myapp.Services.ElasticsearchService do
   def search(path, key, value),
     do: format_response(Tirexs.HTTP.get("#{path}/_search?q=#{key}:#{value}"))
 
+  def list(path), do: list(path, @max_limit)
+
+  def list(path, limit) when limit > @max_limit, do: list(path, @max_limit)
+
+  def list(path, limit), do: format_response(Tirexs.HTTP.post("#{path}/_search", %{size: limit}))
+
   defp format_response({:ok, 200, %{:hits => %{:hits => hits_list}}}),
     do: {:ok, Enum.map(hits_list, fn x -> x[:_source] end)}
 
-  defp format_response(any), do: {:error, :bad_request}
+  defp format_response(any), do: {:error, any}
 end
