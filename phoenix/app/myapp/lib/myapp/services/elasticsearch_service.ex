5df17d4d120c8df655c3b497428245cd7b398ace
Bruno Brasil <bruno.brasil@b2wdigital.com>
2022-01-05 16:12:24 -0300

    elasticsearch service refactor

--- phoenix/app/myapp/lib/myapp/services/elasticsearch_service.ex
+++ phoenix/app/myapp/lib/myapp/services/elasticsearch_service.ex
@@ -10,28 +10,25 @@ defmodule Myapp.Services.ElasticsearchService do
 
   def get_index(), do: @index
 
-  def post(path, data) do
-    path
-    |> get_path_with_index()
-    |> Tirexs.HTTP.post(data)
+  def post(document, data) do
+    document
+    |> tirexs_post(data)
     |> format_response()
   end
 
-  def search(path, key, value) do
-    path
-    |> get_path_with_index()
-    |> tirexs_search_key_value(key, value)
+  def search(document, key, value) do
+    document
+    |> tirexs_search(key, value)
     |> format_response()
   end
 
-  def list(path), do: list(path, @max_limit)
+  def list(document), do: list(document, @max_limit)
 
-  def list(path, limit) when limit > @max_limit, do: list(path, @max_limit)
+  def list(document, limit) when limit > @max_limit, do: list(document, @max_limit)
 
-  def list(path, limit) do
-    path
-    |> get_search_path()
-    |> Tirexs.HTTP.post(%{size: limit})
+  def list(document, limit) do
+    document
+    |> tirexs_search(%{size: limit})
     |> format_response()
   end
 
@@ -40,29 +37,27 @@ defmodule Myapp.Services.ElasticsearchService do
     Tirexs.HTTP.put(@index)
   end
 
-  def delete(path, key, value) do
-    with full_path <- get_path_with_index(path),
-         els_id <- search_and_get_els_id(full_path, key, value),
-         {:ok, 200, details} <- tirexs_delete_by_els_id(full_path, els_id) do
+  def delete(document, key, value) do
+    with els_id <- search_and_get_els_id(document, key, value),
+         {:ok, 200, details} <- tirexs_delete_by_els_id(document, els_id) do
       format_response({:ok, 200, details})
     else
       error -> {:error, error}
     end
   end
 
-  def update(path, key, value, new_data) do
-    with full_path <- get_path_with_index(path),
-         els_id <- search_and_get_els_id(full_path, key, value),
-         {:ok, 200, details} <- Tirexs.HTTP.post("#{full_path}/#{els_id}", new_data) do
+  def update(document, key, value, new_data) do
+    with els_id <- search_and_get_els_id(document, key, value),
+         {:ok, 200, details} <- tirexs_update_by_els_id(document, els_id, new_data) do
       format_response({:ok, 200, details})
     else
       error -> {:error, error}
     end
   end
 
-  defp search_and_get_els_id(full_path, key, value) do
-    full_path
-    |> tirexs_search_key_value(key, value)
+  defp search_and_get_els_id(document, key, value) do
+    document
+    |> tirexs_search(key, value)
     |> format_response_get_els_id()
     |> List.first()
   end
@@ -77,13 +72,23 @@ defmodule Myapp.Services.ElasticsearchService do
   defp format_response_get_els_id({:ok, 200, %{:hits => %{:hits => hits_list}}}),
     do: Enum.map(hits_list, fn x -> x[:_id] end)
 
-  defp get_path_with_index(path), do: "#{@index}/#{path}"
+  defp get_full_path(document), do: "#{@index}/#{document}"
 
-  defp get_search_path(path), do: "#{@index}/#{path}/_search"
+  defp tirexs_post(document, data),
+    do: Tirexs.HTTP.post("#{get_full_path(document)}", data)
 
-  defp tirexs_search_key_value(full_path, key, value),
-    do: Tirexs.HTTP.get("#{full_path}/_search?q=#{key}:#{value}")
+  defp tirexs_search(document, key, value, body),
+    do: Tirexs.HTTP.post("#{get_full_path(document)}/_search?q=#{key}:#{value}", body)
 
-  defp tirexs_delete_by_els_id(full_path, els_id),
-    do: Tirexs.HTTP.delete("#{full_path}/#{els_id}")
+  defp tirexs_search(document, body),
+    do: Tirexs.HTTP.post("#{get_full_path(document)}/_search", body)
+
+  defp tirexs_search(document, key, value),
+    do: Tirexs.HTTP.get("#{get_full_path(document)}/_search?q=#{key}:#{value}")
+
+  defp tirexs_delete_by_els_id(document, els_id),
+    do: Tirexs.HTTP.delete("#{get_full_path(document)}/#{els_id}")
+
+  defp tirexs_update_by_els_id(document, els_id, new_data),
+    do: Tirexs.HTTP.post("#{get_full_path(document)}/#{els_id}", new_data)
 end
--- phoenix/app/myapp/test/myapp/services/elasticsearch_service_test.exs
+++ phoenix/app/myapp/test/myapp/services/elasticsearch_service_test.exs
@@ -7,8 +7,8 @@ defmodule Myapp.Services.ElasticsearchServiceTest do
 
   setup_all do
     %{
-      path: "path",
-      full_path: ElasticsearchService.get_index() <> "/path",
+      document: "document",
+      full_path: ElasticsearchService.get_index() <> "/document",
       data: "valid_data",
       key: "valid_key",
       value: "valid_value"
@@ -16,8 +16,8 @@ defmodule Myapp.Services.ElasticsearchServiceTest do
   end
 
   setup_with_mocks([
-    {Tirexs.HTTP, [], post: fn _path, data -> els_sucessful_response_mock(data) end},
-    {Tirexs.HTTP, [], get: fn _path -> els_sucessful_response_mock("search_result") end},
+    {Tirexs.HTTP, [], post: fn _document, data -> els_sucessful_response_mock(data) end},
+    {Tirexs.HTTP, [], get: fn _document -> els_sucessful_response_mock("search_result") end},
     {Tirexs.HTTP, [], delete: fn _index -> els_sucessful_response_mock() end},
     {Tirexs.HTTP, [], put: fn _index -> els_sucessful_response_mock() end}
   ]) do
@@ -25,41 +25,41 @@ defmodule Myapp.Services.ElasticsearchServiceTest do
   end
 
   describe "post" do
-    test "post data on elasticsearch", %{path: path, data: data, full_path: full_path} do
-      assert ElasticsearchService.post(path, data) == {:ok, [data]}
+    test "post data on elasticsearch", %{document: document, data: data, full_path: full_path} do
+      assert ElasticsearchService.post(document, data) == {:ok, [data]}
       assert_called(Tirexs.HTTP.post(full_path, data))
     end
   end
 
   describe "search" do
     test "search data on elasticsearch", %{
-      path: path,
+      document: document,
       full_path: full_path,
       key: key,
       value: value
     } do
-      assert ElasticsearchService.search(path, key, value) == {:ok, ["search_result"]}
+      assert ElasticsearchService.search(document, key, value) == {:ok, ["search_result"]}
       assert_called(Tirexs.HTTP.get("#{full_path}/_search?q=#{key}:#{value}"))
     end
   end
 
   describe "list" do
     test "list data from elasticsearch", %{
-      path: path,
+      document: document,
       full_path: full_path
     } do
       size = 1000
-      assert ElasticsearchService.list(path, size) == {:ok, [%{size: size}]}
+      assert ElasticsearchService.list(document, size) == {:ok, [%{size: size}]}
       assert_called(Tirexs.HTTP.post("#{full_path}/_search", %{size: size}))
     end
 
     test "limit size to 10000", %{
-      path: path,
+      document: document,
       full_path: full_path
     } do
       invalid_size = 999_999
       max_size = 10_000
-      assert ElasticsearchService.list(path, invalid_size) == {:ok, [%{size: max_size}]}
+      assert ElasticsearchService.list(document, invalid_size) == {:ok, [%{size: max_size}]}
       assert_called(Tirexs.HTTP.post("#{full_path}/_search", %{size: max_size}))
     end
   end
@@ -75,22 +75,22 @@ defmodule Myapp.Services.ElasticsearchServiceTest do
 
   describe "delete" do
     test "deletes item found on search", %{
-      path: path,
+      document: document,
       key: key,
       value: value
     } do
-      assert ElasticsearchService.delete(path, key, value) == {:ok, []}
+      assert ElasticsearchService.delete(document, key, value) == {:ok, []}
       assert_called(Tirexs.HTTP.delete(:_))
     end
   end
 
   describe "update" do
     test "updates item found on search", %{
-      path: path,
+      document: document,
       key: key,
       value: value
     } do
-      assert ElasticsearchService.update(path, key, value, "data") == {:ok, ["data"]}
+      assert ElasticsearchService.update(document, key, value, "data") == {:ok, ["data"]}
       assert_called(Tirexs.HTTP.post(:_, "data"))
     end
   end
