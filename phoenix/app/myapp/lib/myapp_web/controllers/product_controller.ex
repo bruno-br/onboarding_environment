2a13cdd116ed3680d68dc35ad872bdc30a3899e8
Bruno Brasil <bruno.brasil@b2wdigital.com>
2022-01-10 13:57:39 -0300

    render products on fallback controller

--- phoenix/app/myapp/lib/myapp_web/controllers/product_controller.ex
+++ phoenix/app/myapp/lib/myapp_web/controllers/product_controller.ex
@@ -10,44 +10,40 @@ defmodule MyappWeb.ProductController do
 
   action_fallback(MyappWeb.FallbackController)
 
-  def index(conn, params) do
-    params
-    |> Map.to_list()
-    |> Management.list_products()
-    |> send_products_list(conn)
+  def index(_conn, params) do
+    products =
+      params
+      |> Map.to_list()
+      |> Management.list_products()
+
+    {:index, products}
   end
 
-  def create(conn, %{"product" => product_params}) do
+  def create(_conn, %{"product" => product_params}) do
     case Management.create_product(product_params) do
       {:ok, %Product{} = product} ->
-        conn
-        |> put_status(:created)
-        |> render("show.json", product: product)
+        {:created, product}
 
       error ->
         error
     end
   end
 
-  def show(conn, %{"id" => _id}), do: send_product(conn, conn.assigns[:product])
+  def show(conn, %{"id" => _id}), do: {:ok, conn.assigns[:product]}
 
   def update(conn, %{"id" => _id, "product" => product_params}) do
     with %Product{} = product <- conn.assigns[:product],
          {:ok, %Product{} = updated_product} <- Management.update_product(product, product_params) do
-      send_product(conn, updated_product)
+      {:ok, updated_product}
     end
   end
 
   def delete(conn, %{"id" => _id}) do
     with %Product{} = product <- conn.assigns[:product],
          {:ok, %Product{}} <- Management.delete_product(product) do
-      send_resp(conn, :no_content, "")
+      :no_content
     else
       error -> error
     end
   end
-
-  defp send_product(conn, product), do: render(conn, "show.json", product: product)
-
-  defp send_products_list(products, conn), do: render(conn, "index.json", products: products)
 end
