47216b99fc2916394ff8be2d630a435320e4ebf7
Bruno Brasil <bruno.brasil@b2wdigital.com>
2022-01-18 16:30:36 -0300

    test if GenerateReportWorker updates report status and data on redis

--- phoenix/app/myapp/test/myapp/workers/generate_report_worker_test.exs
+++ phoenix/app/myapp/test/myapp/workers/generate_report_worker_test.exs
@@ -9,10 +9,12 @@ defmodule Myapp.Workers.GenerateReportWorkerTest do
 
   def get_list_function_mock(), do: [%{a: 1}, %{a: 2}]
 
+  @csv_formated "list_csv"
+
   setup_with_mocks([
     {RedisService, [], start: fn -> "redis_client" end},
     {RedisService, [], set: fn _redis_client, _key, _data -> :ok end},
-    {CsvFormatService, [], get_csv_string: fn _list -> "list_csv" end}
+    {CsvFormatService, [], get_csv_string: fn _list -> @csv_formated end}
   ]) do
     :ok
   end
@@ -21,6 +23,17 @@ defmodule Myapp.Workers.GenerateReportWorkerTest do
     test "returns :ok if report was sucessfully generated" do
       {report_title, get_list_function} = get_report_mock_data()
       assert GenerateReportWorker.perform(report_title, get_list_function) == :ok
+    end
+
+    test "updates report status and data on redis" do
+      {report_title, get_list_function} = get_report_mock_data()
+      GenerateReportWorker.perform(report_title, get_list_function)
+
+      report_value_generating = %{data: "", status: :generating}
+      assert_called(RedisService.set("redis_client", report_title, report_value_generating))
+
+      report_value_completed = %{data: @csv_formated, status: :completed}
+      assert_called(RedisService.set("redis_client", report_title, report_value_completed))
     end
   end
 
