3f9421c2b7773f94f9d4184be96b9c0c0d1a6908
Bruno Brasil <bruno.brasil@b2wdigital.com>
2022-01-19 16:08:35 -0300

    test if GenerateReportWorker creates new csv file with correct name and data

--- phoenix/app/myapp/test/myapp/workers/generate_report_worker_test.exs
+++ phoenix/app/myapp/test/myapp/workers/generate_report_worker_test.exs
@@ -7,13 +7,15 @@ defmodule Myapp.Workers.GenerateReportWorkerTest do
   alias Myapp.Services.CsvFormatService
   alias Myapp.Workers.GenerateReportWorker
 
+  @csv_formated "list_csv"
+
   def get_list_function_mock(), do: [%{a: 1}, %{a: 2}]
 
   setup_with_mocks([
     {RedisService, [], start: fn -> "redis_client" end},
     {RedisService, [], set: fn _redis_client, _key, _data -> :ok end},
     {RedisService, [], del: fn _redis_client, _key -> :ok end},
-    {CsvFormatService, [], get_csv_string: fn _list -> "list_csv" end},
+    {CsvFormatService, [], get_csv_string: fn _list -> @csv_formated end},
     {File, [], write: fn _path, _data -> :ok end}
   ]) do
     :ok
@@ -50,6 +52,16 @@ defmodule Myapp.Workers.GenerateReportWorkerTest do
       GenerateReportWorker.perform(report_title, "invalid_arg")
 
       assert_called(RedisService.del("redis_client", report_status_key))
+    end
+
+    test "creates new csv file with correct name and data" do
+      {report_title, get_list_function} = get_report_mock_data()
+      expected_file_name = "#{report_title}.csv"
+      expected_file_data = @csv_formated
+
+      GenerateReportWorker.perform(report_title, get_list_function)
+
+      assert_called(File.write(expected_file_name, expected_file_data))
     end
   end
 
