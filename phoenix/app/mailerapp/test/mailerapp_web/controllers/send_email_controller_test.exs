e660b71a96fe14820d4696c848490c699cf6facb
Bruno Brasil <bruno.brasil@b2wdigital.com>
2022-01-31 15:19:48 -0300

    send_email_controller test calls with post

--- phoenix/app/mailerapp/test/mailerapp_web/controllers/send_email_controller_test.exs
+++ phoenix/app/mailerapp/test/mailerapp_web/controllers/send_email_controller_test.exs
@@ -13,7 +13,7 @@ defmodule MailerAppWeb.SendEmailControllerTest do
     test_with_mock("returns 200 status code", %{conn: conn}, SendEmailService, [],
       send: fn _data -> {:ok, "message"} end
     ) do
-      conn = get(conn, Routes.send_email_path(conn, :index))
+      conn = post(conn, Routes.send_email_path(conn, :handle))
       response(conn, 200)
     end
   end
