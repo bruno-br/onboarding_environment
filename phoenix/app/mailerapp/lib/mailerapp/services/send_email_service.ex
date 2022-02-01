d5ac1053f1b14c85556420908e9429764aa1b03b
Bruno Brasil <bruno.brasil@b2wdigital.com>
2022-02-01 15:09:39 -0300

    convert param keys from string to atoms on send_email_service

--- phoenix/app/mailerapp/lib/mailerapp/services/send_email_service.ex
+++ phoenix/app/mailerapp/lib/mailerapp/services/send_email_service.ex
@@ -1,8 +1,13 @@
 defmodule MailerApp.Services.SendEmailService do
   alias MailerApp.{Email, Mailer}
 
-  def send(%{from: from, html_body: html_body, subject: subject, text_body: text_body, to: to} = email_params) do
-    Email.create(email_params) |> Mailer.deliver_later()
+  def send(email_params) do
+    email_params
+    |> map_string_keys_to_atoms()
+    |> Email.create()
+    |> Mailer.deliver_later()
     {:ok, "Email sent successfully"}
   end
+
+  defp map_string_keys_to_atoms(map_string_keys), do: for {key, val} <- map_string_keys, into: %{}, do: {String.to_existing_atom(key), val}
 end
--- phoenix/app/mailerapp/lib/mailerapp_web/controllers/send_email_controller.ex
+++ phoenix/app/mailerapp/lib/mailerapp_web/controllers/send_email_controller.ex
@@ -5,10 +5,7 @@ defmodule MailerAppWeb.SendEmailController do
   alias MailerApp.Services.SendEmailService
 
   def handle(conn, params) do
-
-    atom_params = for {key, val} <- params, into: %{}, do: {String.to_existing_atom(key), val}
-
-    case SendEmailService.send(atom_params) do
+  case SendEmailService.send(params) do
       {:ok, message}  -> send_resp(conn, :ok, message)
       _ -> send_resp(conn, :internal_server_error, "There was an error trying to send the email")
     end
