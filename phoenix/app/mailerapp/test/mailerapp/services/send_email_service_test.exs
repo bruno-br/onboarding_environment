0f5cf3a9f1fbf4e7d3f21994c7355b9349bfa338
Bruno Brasil <bruno.brasil@b2wdigital.com>
2022-02-01 15:44:29 -0300

    test if send_email_service returns success message if there are no errors

---
+++ phoenix/app/mailerapp/test/mailerapp/services/send_email_service_test.exs
@@ -0,0 +1,30 @@
+defmodule MailerApp.Services.SendEmailServiceTest do
+  use MailerApp.DataCase, async: false
+
+  alias MailerApp.{Email, Mailer}
+  alias MailerApp.Services.SendEmailService
+
+  import Mock
+
+  @email_params_atom %{key: "value"}
+  @email_params_string %{"key" => "value"}
+  @valid_email "email"
+
+  setup_with_mocks([
+    {Email, [],
+     create: fn
+       @email_params_atom -> @valid_email
+       _invalid_params -> :error
+     end},
+    {Mailer, [], deliver_later: fn @valid_email -> :ok end}
+  ]) do
+    :ok
+  end
+
+  describe "send/1" do
+    test "returns success message if there were no errors" do
+      expected_response = {:ok, "Email sent successfully"}
+      assert SendEmailService.send(@email_params_string) == expected_response
+    end
+  end
+end
