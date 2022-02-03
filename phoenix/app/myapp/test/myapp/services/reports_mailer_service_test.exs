34b763db4a82c7289a225d34a2bfb927984cf8c4
Bruno Brasil <bruno.brasil@b2wdigital.com>
2022-02-03 17:42:07 -0300

    test if ReportsMailerService returns response from MailerService.send_email/1

--- phoenix/app/myapp/test/myapp/services/reports_mailer_service_test.exs
+++ phoenix/app/myapp/test/myapp/services/reports_mailer_service_test.exs
@@ -5,10 +5,12 @@ defmodule Myapp.Services.ReportsMailerServiceTest do
 
   import Mock
 
+  @success_response {:ok, "Email sent successfully"}
+
   setup_with_mocks([
     {MailerService, [],
      send_email: fn
-       _body -> {:ok, "Email sent successfully"}
+       _body -> @success_response
      end}
   ]) do
     :ok
@@ -18,6 +20,10 @@ defmodule Myapp.Services.ReportsMailerServiceTest do
     test "calls MailerService.send_email/1" do
       ReportsMailerService.send_email()
       assert_called(MailerService.send_email(:_))
+    end
+
+    test "returns response from MailerService.send_email/1" do
+      assert ReportsMailerService.send_email() == @success_response
     end
   end
 end
