5b5078f0b26d2861523e7d7519649469ea75333e
Bruno Brasil <bruno.brasil@b2wdigital.com>
2022-02-04 17:06:42 -0300

    get all information about report and attachment

--- phoenix/app/myapp/lib/myapp/services/reports_mailer_service.ex
+++ phoenix/app/myapp/lib/myapp/services/reports_mailer_service.ex
@@ -1,18 +1,14 @@
 defmodule Myapp.Services.ReportsMailerService do
   alias Myapp.Services.MailerService
 
-  def send_email(report_data, report_title) do
+  def send_email(title, %{content_type: _content_type, filename: _filename, data: _data} = attachment) do
     body = %{
       from: "hattie.block86@ethereal.email",
       to: "ascrvf7j5vfyc46y@ethereal.email",
-      subject: "Report Mail",
+      subject: title,
       text_body: "This Email contains a Report",
       html_body: "This Email contains a Report",
-      attachment: %{
-        content_type: "text/csv",
-        filename: "#{report_title}.csv",
-        data: report_data
-      }
+      attachment: attachment
     }
     MailerService.send_email(body)
   end
