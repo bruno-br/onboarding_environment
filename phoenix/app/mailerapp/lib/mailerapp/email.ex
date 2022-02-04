b38ea5d1fd0f192504f668369b9a01a15c1aaf26
Bruno Brasil <bruno.brasil@b2wdigital.com>
2022-02-04 13:44:25 -0300

    accept attachments on emails

--- phoenix/app/mailerapp/lib/mailerapp/email.ex
+++ phoenix/app/mailerapp/lib/mailerapp/email.ex
@@ -1,6 +1,24 @@
 defmodule MailerApp.Email do
   import Bamboo.Email
 
+  def create(%{
+        from: from,
+        html_body: html_body,
+        subject: subject,
+        text_body: text_body,
+        to: to,
+        attachment: %{content_type: content_type, filename: filename, data: data}
+      }) do
+    email =
+      create(%{from: from, html_body: html_body, subject: subject, text_body: text_body, to: to})
+
+    put_attachment(email, %Bamboo.Attachment{
+      content_type: content_type,
+      filename: filename,
+      data: data
+    })
+  end
+
   def create(
         %{from: _from, html_body: _html_body, subject: _subject, text_body: _text_body, to: _to} =
           email_params
