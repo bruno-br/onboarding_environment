dfdabaeb0b4c8f45c42b87a34a5ab9df2c879f78
Bruno Brasil <bruno.brasil@b2wdigital.com>
2022-02-07 13:49:48 -0300

    generate_report_worker sends email after generating the report

--- phoenix/app/myapp/lib/myapp/workers/generate_report_worker.ex
+++ phoenix/app/myapp/lib/myapp/workers/generate_report_worker.ex
@@ -1,14 +1,15 @@
 defmodule Myapp.Workers.GenerateReportWorker do
-  alias Myapp.Services.RedisService
-  alias Myapp.Services.CsvFormatService
+  alias Myapp.Services.{CsvFormatService, RedisService, ReportsMailerService}
 
   def perform(report_title, get_list_function, expiration_in_seconds \\ 60) do
     with :ok <- set_report_status(report_title, :generating, expiration_in_seconds),
          list <- get_list(get_list_function),
          {:ok, report_data} <- generate_report_data(list),
          :ok <- save_report(report_title, report_data),
-         :ok <- set_report_status(report_title, :completed, expiration_in_seconds) do
-      :ok
+         :ok <- set_report_status(report_title, :completed, expiration_in_seconds),
+         {:ok, _message} <- send_report_email(report_title, report_data)
+        do
+        :ok
     else
       _error ->
         delete_report_status(report_title)
@@ -41,4 +42,8 @@ defmodule Myapp.Workers.GenerateReportWorker do
   end
 
   defp get_list(_invalid_params), do: :error
+
+  defp send_report_email(report_title, report_data) do
+    ReportsMailerService.send_email(report_title, %{content_type: "text/csv", filename: "#{report_title}.csv", data: report_data})
+  end
 end
