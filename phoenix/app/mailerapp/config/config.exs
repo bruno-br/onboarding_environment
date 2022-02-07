963bbd4dbd129703b392f9d0de09ddb16b19f9be
Bruno Brasil <bruno.brasil@b2wdigital.com>
2022-02-07 17:02:34 -0300

    remove comments

--- phoenix/app/mailerapp/config/config.exs
+++ phoenix/app/mailerapp/config/config.exs
@@ -1,14 +1,4 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
-
-# General application configuration
 use Mix.Config
-
-# config :mailerapp,
-#   ecto_repos: [MailerApp.Repo]
 
 # Configures the endpoint
 config :mailerapp, MailerAppWeb.Endpoint,
--- phoenix/app/mailerapp/config/prod.secret.exs
+++ phoenix/app/mailerapp/config/prod.secret.exs
@@ -22,13 +22,3 @@ config :mailerapp, MailerAppWeb.Endpoint,
     transport_options: [socket_opts: [:inet6]]
   ],
   secret_key_base: secret_key_base
-
-# ## Using releases (Elixir v1.9+)
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start each relevant endpoint:
-#
-#     config :mailerapp, MailerAppWeb.Endpoint, server: true
-#
-# Then you can assemble a release by calling `mix release`.
-# See `mix help release` for more information.
