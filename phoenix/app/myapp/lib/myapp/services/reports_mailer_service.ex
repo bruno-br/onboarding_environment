c98cba87fd2e2b645a787c7883abbd332e155ec7
Bruno Brasil <bruno.brasil@b2wdigital.com>
2022-02-07 11:44:17 -0300

    encode attachment data to base16 before sending

--- phoenix/app/myapp/lib/myapp/services/reports_mailer_service.ex
+++ phoenix/app/myapp/lib/myapp/services/reports_mailer_service.ex
@@ -1,7 +1,11 @@
 defmodule Myapp.Services.ReportsMailerService do
   alias Myapp.Services.MailerService
 
-  def send_email(title, %{content_type: _content_type, filename: _filename, data: _data} = attachment) do
+  def send_email(title, %{content_type: _content_type, filename: _filename, data: data} = attachment) do
+
+    encoded_data = Base.encode16(data)
+    attachment = %{attachment | data: encoded_data}
+
     body = %{
       from: "hattie.block86@ethereal.email",
       to: "ascrvf7j5vfyc46y@ethereal.email",
