96efd1a4ed6a404edff18acdb860fdb091f18ff7
Bruno Brasil <bruno.brasil@b2wdigital.com>
2022-02-08 14:06:55 -0300

    encode request body to json using poison

--- phoenix/app/myapp/lib/myapp/services/mailer_service.ex
+++ phoenix/app/myapp/lib/myapp/services/mailer_service.ex
@@ -2,9 +2,17 @@ defmodule Myapp.Services.MailerService do
   def send_email(body) do
     url = get_send_email_url()
     headers = get_default_headers()
-    encoded_body = encode_body(body)
 
-    case HTTPoison.post(url, encoded_body, headers, []) do
+    with {:ok, encoded_body} <- encode_body(body),
+         {:ok, message} <- post_mail(url, encoded_body, headers) do
+      {:ok, message}
+    else
+      error -> error
+    end
+  end
+
+  defp post_mail(url, body, headers) do
+    case HTTPoison.post(url, body, headers, []) do
       {:ok, %HTTPoison.Response{body: response_body, status_code: 202}} -> {:ok, response_body}
       {:ok, %HTTPoison.Response{body: response_body}} -> {:error, response_body}
       error -> error
@@ -17,16 +25,5 @@ defmodule Myapp.Services.MailerService do
 
   defp get_default_headers(), do: [{"Content-type", "application/json"}]
 
-  defp encode_body(%{} = map), do: format_keys_and_values(map)
-
-  defp format_keys_and_values(%{} = map),
-    do:
-      "{" <>
-        Enum.map_join(map, ", ", fn {key, val} -> "\"#{key}\": #{format_keys_and_values(val)}" end) <>
-        "}"
-
-  defp format_keys_and_values(not_a_map),
-    do: "\"#{format_value(not_a_map)}\""
-
-  defp format_value(value), do: String.replace(value, "\"", "\\\"")
+  defp encode_body(body), do: Poison.encode(body)
 end
