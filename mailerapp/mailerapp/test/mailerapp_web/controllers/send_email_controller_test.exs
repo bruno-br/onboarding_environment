cabf725a115b4a8b2827d2518d2b4c046b845c97
Bruno Brasil <bruno.brasil@b2wdigital.com>
2022-02-10 13:43:49 -0300

    replace constants with setup_all

--- mailerapp/mailerapp/test/mailerapp_web/controllers/send_email_controller_test.exs
+++ mailerapp/mailerapp/test/mailerapp_web/controllers/send_email_controller_test.exs
@@ -5,25 +5,26 @@ defmodule MailerAppWeb.SendEmailControllerTest do
 
   import Mock
 
-  @send_email_params %{"from" => "from@mail.com", "to" => "to@mail.com"}
-  @send_email_error_response {:error, "There was an error trying to send the email"}
-  @send_email_success_response {:accepted, "The Email is going to be sent"}
-
   setup %{conn: conn} do
-    [conn: put_req_header(conn, "accept", "application/json")]
+    [
+      conn: put_req_header(conn, "accept", "application/json"),
+      send_email_params: %{"from" => "from@mail.com", "to" => "to@mail.com"},
+      send_email_error_response: {:error, "There was an error trying to send the email"},
+      send_email_success_response: {:accepted, "The Email is going to be sent"}
+    ]
   end
 
   describe "handle/2" do
     test_with_mock(
       "returns error message if there is an error on SendEmailService.send",
-      %{conn: conn},
+      %{conn: conn, send_email_error_response: send_email_error_response},
       SendEmailService,
       [],
-      send: fn _data -> @send_email_error_response end
+      send: fn _data -> send_email_error_response end
     ) do
       conn = post(conn, Routes.send_email_path(conn, :handle))
 
-      {:error, expected_response} = @send_email_error_response
+      {:error, expected_response} = send_email_error_response
 
       assert response(conn, 500) == expected_response
     end
@@ -31,16 +32,20 @@ defmodule MailerAppWeb.SendEmailControllerTest do
 
   test_with_mock(
     "send correct params to SendEmailService.send and return success response",
-    %{conn: conn},
+    %{
+      conn: conn,
+      send_email_params: send_email_params,
+      send_email_success_response: send_email_success_response
+    },
     SendEmailService,
     [],
-    send: fn _data -> @send_email_success_response end
+    send: fn _data -> send_email_success_response end
   ) do
-    conn = post(conn, Routes.send_email_path(conn, :handle), @send_email_params)
+    conn = post(conn, Routes.send_email_path(conn, :handle), send_email_params)
 
-    assert_called(SendEmailService.send(@send_email_params))
+    assert_called(SendEmailService.send(send_email_params))
 
-    {:accepted, expected_response} = @send_email_success_response
+    {:accepted, expected_response} = send_email_success_response
 
     assert response(conn, 202) == expected_response
   end
