b1d91b91cd119f5cc6300ceb8c85b4f706c7a8aa
Bruno Brasil <bruno.brasil@b2wdigital.com>
2022-02-10 11:55:44 -0300

    use setup_all

--- mailerapp/mailerapp/test/mailerapp/email_test.exs
+++ mailerapp/mailerapp/test/mailerapp/email_test.exs
@@ -5,23 +5,38 @@ defmodule MailerApp.EmailTest do
 
   import Mock
 
-  @email_params %{
-    from: "from",
-    html_body: "html_body",
-    subject: "subject",
-    text_body: "text_body",
-    to: "to"
-  }
-  @attachment %{content_type: "text/csv", filename: "file.csv", data: "encoded_data"}
-  @email_params_with_attachment Map.merge(@email_params, %{attachment: @attachment})
   @valid_email "valid_email"
   @decoded_data "decoded_data"
+
+  setup_all do
+    %{
+      email_params: %{
+        from: "from",
+        html_body: "html_body",
+        subject: "subject",
+        text_body: "text_body",
+        to: "to"
+      },
+      email_params_with_attachment: %{
+        from: "from",
+        html_body: "html_body",
+        subject: "subject",
+        text_body: "text_body",
+        to: "to",
+        attachment: %{
+          content_type: "text/csv",
+          filename: "file.csv",
+          data: "encoded_data"
+        }
+      }
+    }
+  end
 
   setup_with_mocks([
     {
       Bamboo.Email,
       [],
-      new_email: fn @email_params -> @valid_email end,
+      new_email: fn _email_params -> @valid_email end,
       put_attachment: fn _email, _attachment -> @valid_email end
     },
     {Base, [], decode16: fn _encoded_data -> {:ok, @decoded_data} end}
@@ -30,27 +45,32 @@ defmodule MailerApp.EmailTest do
   end
 
   describe "create/1" do
-    test("returns valid_email on success") do
-      assert Email.create(@email_params) == @valid_email
-      assert_called(Bamboo.Email.new_email(@email_params))
+    test("returns valid_email on success", %{email_params: email_params}) do
+      assert Email.create(email_params) == @valid_email
+      assert_called(Bamboo.Email.new_email(email_params))
       assert_not_called(Bamboo.Email.put_attachment(@valid_email, :_))
     end
 
-    test("calls Bamboo.Email.put_attachment/2 when an attachment is sent on params") do
-      Email.create(@email_params_with_attachment)
+    test("calls Bamboo.Email.put_attachment/2 when an attachment is sent on params", %{email_params_with_attachment: email_params_with_attachment}) do
+      Email.create(email_params_with_attachment)
+
+      attachment = email_params_with_attachment[:attachment]
 
       bamboo_attachment = %Bamboo.Attachment{
-        content_type: @attachment[:content_type],
-        filename: @attachment[:filename],
+        content_type: attachment[:content_type],
+        filename: attachment[:filename],
         data: @decoded_data
       }
 
       assert_called(Bamboo.Email.put_attachment(@valid_email, bamboo_attachment))
     end
 
-    test("decodes attachment data") do
-      Email.create(@email_params_with_attachment)
-      assert_called(Base.decode16(@attachment[:data]))
+    test("decodes attachment data", %{email_params_with_attachment: email_params_with_attachment}) do
+      Email.create(email_params_with_attachment)
+
+      attachment = email_params_with_attachment[:attachment]
+
+      assert_called(Base.decode16(attachment[:data]))
     end
   end
 end
